67bca22404859c37bd5c64dc4a50af6806039e24
JohnnyBravo75 <tomek_29@gmx.de>
2017-09-14 01:29:12 +0200

    -ADD: Converters for fields

---
+++ DataConnectors/Converters/DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace DataConnectors.Converters
+{
+    public class DateTimeConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string)
+            {
+                if (parameter is string && !string.IsNullOrEmpty(parameter as string))
+                {
+                    DateTime returnDate;
+                    DateTime.TryParseExact(value as string, parameter as string, culture, DateTimeStyles.None, out returnDate);
+                }
+            }
+            return value;
+        }
+    }
+}
